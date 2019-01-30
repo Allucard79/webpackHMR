1a8939153e439e25a6924a706a3ef6ad793f7e64
Marcin Ratajczak <maratajczak@gmail.com>
2019-01-30 17:00:28 +0100

    Fix warning msg

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -4,7 +4,7 @@ import style from './TodoList.css'
 
 const TodoList = props => {
 	const items = props.items.map(item =>
-		<Todo item={item} remove={props.remove} />
+		<Todo item={item} key={item.id} remove={props.remove} />
 	);
 	return <ul>{items}</ul>
 }
